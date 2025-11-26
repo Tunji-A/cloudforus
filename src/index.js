33bbe61fd07c4d13dd5c45ea3ede89e351d0ac35
TUNJI <oy.amuzat@gmail.com>
2025-11-27 00:36:03 +0100

    update website

--- src/index.js
+++ src/index.js
@@ -1,15 +1,11 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { createRoot } from "react-dom/client";
+import "./App.css";
 import App from "./App";
-import "./index.css";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-
+const root = createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <App />
   </React.StrictMode>
 );
